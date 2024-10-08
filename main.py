a8041330753671c487ad1ae6554171d33d4905ad
Nicole1942 <schmidtnicole950@gmail.com>
2024-10-09 00:38:40 +0200

    Added Scoring System

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ def main():
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
 
+  score = 0 
+
   pygame.init
+  pygame.font.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  font = pygame.font.Font(None, 36)
 
   clock = pygame.time.Clock()
   dt = 0
@@ -46,6 +50,9 @@ def main():
       for shot in shots:
          if asteroid.check_collision(shot):
             asteroid.split()
+            score += 10
+    score_text = font.render(f"Score: {score}", True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
